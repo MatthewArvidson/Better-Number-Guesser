1bb4c02cbaf01acb2e9d5beb7aa631dd240424a1
Matthew Arvidson <Matthew.Arvidson@gmail.com>
2017-08-22 17:12:17 -0600

    Completes phase one

--- java.js
+++ java.js
@@ -1,4 +1,6 @@
-//Global Variables
+//****Global Variables****
+
+
 var alertDisplay =  document.querySelector(".alert-display")
 var guess;
 var guessButton = document.querySelector(".guess-button");
@@ -8,83 +10,72 @@ var resetButton = document.querySelector(".reset-button");
 var userInput = document.querySelector(".user-input");
 var clearButton = document.querySelector(".clear-button");
 var allButtons = document.querySelectorAll("button");
-console.log(allButtons);
+var alertDisplay = document.querySelector(".alert-display")
+var verifyIsInteger = document.querySelector(".user-input")
 
-//Functions
-//Generate a Random Number
+
+//****Event Listeners****
+
+
+userInput.addEventListener('keyup', activateButtons);
+guessButton.addEventListener('click', storeGuess);
+clearButton.addEventListener('click', clearInput);
+resetButton.addEventListener('click', function(){
+  window.location.reload(true);
+});
+
+//****Functions****
+
+
+//Verify The User Input Is An Integer
+function verifyIsInteger() {
+  Number.isInteger(userInput);
+}
+console.log(verifyIsInteger)
+
+//Generate A Random Number
 function randomNum(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1)) + min;
 };
-
 console.log(randomNum)
 
-allButtons.addEventListener('keyup', activateButtons);
-guessButton.addEventListener('click', storeGuess);
-clearButton.addEventListener('click', clearInput);
-console.log(activateButtons);
-
-
-//Take User Input and Compare it to the Random Number
-
+//Activate Buttons
 function activateButtons() {
-  if (userInput.value !== " ") {
-    allButtons.attr("disabled", true);
-  } else {
-    allButtons.attr("disabled", false)
+  if (!userInput.value) {
+    guessButton.disabled = true;
+    clearButton.disabled = true;
+    resetButton.disabled = true;
+  }
+  else {
+    guessButton.disabled = false;
+    clearButton.disabled = false;
+    resetButton.disabled = false;
   }
 };
 
+//Store Guess
 function storeGuess() {
   guess = parseInt(userInput.value);
   lastGuess.innerHTML = guess;
   checkGuess();
 }
 
+//Check Guess Against Random Number
 function checkGuess () {
   if (userInput.value < randomNum) {
-    alert("You Guessed Too Low Asshole!!")
+    alertDisplay.innerText = "You Guessed Too Low Asshole!!";
   }
   else if (userInput.value > randomNum) {
-    alert("You Guessed Too High You Fucker!!")
+    alertDisplay.innerText = "You Guessed Too High You Fucker!!";
   }
   else {
-    alert("You Win You Loser!!")
+    alertDisplay.innerText = "You Win You Loser!!";
   }
 }
 
+//Clear Button The Input Field
 function clearInput() {
   userInput.value = " ";
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// function userGuess() {
-//   var guessButton = document.querySelector('.user-input');
-// }
-//
-// //Event Listeners
-// button.addEventListener('click', function(event) {
-//   event.preventDefault();
-//
-// })
-//
-// resetButton.addEventListener('click', function(event) {
-//   event.preventDefault();
-//   var userInput =
-//
-// })
